40ca99944e30c374e59bef53f54f142a50cad1a2
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-03-08 21:39:40 +0530

    change

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 app=express();
 port = 3000;
-const http = require('http').createServer(app)
-
 path = require('path');
-http.listen(port,()=>{
+
+const http = require('http').createServer(app)
+const server=app.listen(port,()=>{
     console.log(`http://localhost:${port}`)
 })
+
+const io = require('socket.io')(server)
+
 
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
     res.sendFile('index.html')
 })
+
+let socketsConnected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket){
+    console.log('Socket connected', socket.id)
+    socketsConnected.add(socket.id)
+    io.emit('clients-total',socketsConnected.size)
+
+socket.on('disconnect',()=>{
+    console.log('Socket disconnected',socket.id)
+    socketsConnected.delete(socket.id)
+    io.emit('clients-total',socketsConnected.size)
+})
+
+socket.on('message',(data)=>{
+    socket.broadcast.emit('chat-message',data)
+})
+
+socket.on('feedback',(data)=>{
+    socket.broadcast.emit('feedback',data)
+})
+}
